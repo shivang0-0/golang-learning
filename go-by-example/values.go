4fadfccb7bea560bf0374ac416250bc9497d0968
Shivang <shivang.sharma11@pm.me>
2023-08-06 00:20:38 +0530

    go-by-example day1

---
+++ go-by-example/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go-lang" + "learning")
+	fmt.Println("36 + 33 = ", 36+33)
+	fmt.Println("22.0/7.0 = ", 22.0/7.0)
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
---
+++ go-by-example/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "This is a string"
+	fmt.Println(a)
+
+	var b, c int = 6, 9
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "string declaration using shorthand"
+	fmt.Println(f)
+}
